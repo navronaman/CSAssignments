17121074e046a6ff94ce7da474c17da00a68cf83
navronaman <namanmaulikshah@gmail.com>
2023-10-18 00:36:07 -0400

    Finalized staircasebuilder

--- Assignment4/StaircaseBuilder.java
+++ Assignment4/StaircaseBuilder.java
@@ -15,45 +15,33 @@
 public class StaircaseBuilder {
     
     public static void main(String[] args) {
-        int d = 3;
-        int bricks = 6;
+        int d = Integer.parseInt(args[0]);
+        int bricks = Integer.parseInt(args[1]);
+        char [][] staircase = new char[d][d];
 
-        /*
-        // no of bricks per row
-        int nr = d;
-
-        
-        String [][] staircase = new String[d][d];
-
+        // storing the values
+        // the key is to make sure that we store from the column up
         for (int i = 0; i<staircase.length; i++){
-            for (int j = d-1; j>=0; j = j - 1){
-                for(int k = 0; k<nr; nr = nr - 1){
-                    staircase[i][k] = "X";
+            for (int j = staircase[i].length - 1; j >= 0; j--){
+                if (j >= staircase[i].length - 1 - i && bricks > 0){
+                    staircase[j][i] = 'X';
+                    bricks--;
                 }
-                
+                else {
+                    staircase[j][i] = ' ';
+                }
             }
         }
 
         
-        */
-
-        String [][] staircase = new String[d][d];
-
-        for (int i = 0;i<staircase.length; i++){
-            for (int j = staircase[i].length-1; j < i; j++){
-                staircase[i][j] = "X";
-            }
-        }
-
-
+        // print the array
         for (int i = 0; i<staircase.length; i++){
             for (int j = 0; j<staircase[i].length; j++){
                 System.out.print(staircase[i][j]);
             }
             System.out.println();
         }
+        System.out.println("Bricks remaining: " + bricks);
 
-
-        // WRITE YOUR CODE HERE
     }
 }
