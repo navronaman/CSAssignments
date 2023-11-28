148bd82ef8ac9f87363c011701244f98dc3b6936
navronaman <namanmaulikshah@gmail.com>
2023-11-28 01:46:01 -0500

    Assignment 8 - Further commits

--- DNA2.java
+++ DNA2.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
  * @author Aastha Gandhi
  */
 
-public class DNA {
+public class DNA2 {
 
     // These two instance variables are initialized and populated by 
     // createDatabaseOfProfiles() and readSTRsOfInterest()
@@ -38,7 +38,7 @@ public class DNA {
      *                     sequences
      * @param strsFile     The file with all the STRs of interest
      */
-    public DNA ( String databaseFile, String STRsFile ) {
+    public DNA2 ( String databaseFile, String STRsFile ) {
 
         /*** DO NOT EDIT ***/
         createDatabaseOfProfiles(databaseFile); // Calls createDatabase method to initialize the database array
@@ -74,23 +74,20 @@ public class DNA {
 
         StdIn.setFile(filename); // DO NOT remove this line, keep it as the first line in the method.
 
+        /* WRITE YOUR CODE HERE */
         int p = Integer.parseInt(StdIn.readLine());
 
         database = new Profile[p];
-        // String[][] database2 = new String[p][3];
 
-        for (int i = 0; i<p; i++){
+        for (int i = 0; i < p; i++) {
             String name = StdIn.readLine();
+
             String sequence1 = StdIn.readLine();
+
             String sequence2 = StdIn.readLine();
 
-            // database2[i][0] = name;
-            // database2[i][1] = sequence1;
-            // database2[i][2] = sequence2;
-
-            Profile profilei = new Profile(name, null, null, sequence1, sequence2);
-            database[i] = profilei;
-
+            Profile profile = new Profile (name, null, null, sequence1, sequence2);
+            database[i] = profile;
         }
 
     }
@@ -116,13 +113,15 @@ public class DNA {
 
         StdIn.setFile(filename); // DO NOT remove this line, keep as the first line in the method.
 
-        String firstline = StdIn.readLine();
-        int n = Integer.parseInt(firstline);
+        /* WRITE YOUR CODE HERE */
+        int s = Integer.parseInt(StdIn.readLine());
+        STRsOfInterest = new String[s];
+        
+        
+        
 
-        STRsOfInterest = new String[n];
-
-        for (int str = 0; str<n; str++){
-            STRsOfInterest[str] = StdIn.readLine();
+        for(int i = 0; i < s; i++) {
+            STRsOfInterest[i] = StdIn.readLine();
         }
 
     }
@@ -145,14 +144,16 @@ public class DNA {
      */
     public Profile createUnknownProfile ( String filename ) {
 
-	    StdIn.setFile(filename); // DO NOT remove this line, keep as the first line in the method.
+	StdIn.setFile(filename); // DO NOT remove this line, keep as the first line in the method.
 
-        String sequence1 = StdIn.readLine();
-        String sequence2 = StdIn.readLine();
+	/* WRITE YOUR CODE HERE */
+    String sequence1 = StdIn.readLine();
+    String sequence2 = StdIn.readLine();
 
-        Profile Unknown = new Profile("Unknown", null, null, sequence1, sequence2);
+    Profile unknown = new Profile("Unknown", null, null, sequence1, sequence2);
 
-        return Unknown; // update the return value
+
+        return unknown; // update the return value
     }
 
     /**
@@ -165,51 +166,36 @@ public class DNA {
      * @return         The STR object with the name and longest number of repeats
      */
     public STR findSTRInSequence ( String sequence, String STR ) {
-        /*
-        int nor = 0;
 
-        int n = STR.length();
+        /* WRITE YOUR CODE HERE */
+        int maxRepeats = 0;
+        int numOfRepeats = 0;
 
-        for (int i = 0; i<sequence.length()-n+1; i++){
-            String temp = sequence.substring(i, i+n);
-            if(STR.equals(temp)){
-                nor++;
+        for(int i = 0; i < sequence.length(); i++) {
+            if (sequence.startsWith(STR, i)) {
+                numOfRepeats++;
+                i += STR.length() - 1;
+            }
+            else {
+                if (numOfRepeats > maxRepeats) {
+                    maxRepeats = numOfRepeats;
+            }
+            numOfRepeats = 0;
             }
         }
-
-        STR newSTR = new STR(STR, nor);
-
-        return newSTR; // update the return value
-        */
-
-        int nor = 0;
-        int mr = 0;
-
-        int n = STR.length(); // usually 3 or 4
-
-        for (int i = 0; i<sequence.length(); i++){
-            if (sequence.startsWith(STR, i)){
-                nor++;
-                i = n - 1 + i;
-            }
-            else{
-                if (nor>mr){
-                    mr = nor;
-                }
-            nor = 0;
-            }
+        if(numOfRepeats > maxRepeats) {
+            maxRepeats = numOfRepeats;
         }
 
-        if (nor>mr){
-            mr = nor;
-        }
-
-        STR newSTR = new STR(STR, mr);
-
-        return newSTR;
-
-        }
+        
+        return new STR(STR, maxRepeats);
+            
+    }
+ 
+        
     
+
+        
 
     /**
      * Compute the STRs (S1_STRs and S2_STRs) for the profile.
@@ -219,36 +205,43 @@ public class DNA {
      * @param profile The profile of the that the method will compute the STRs array for
      * @param allSTRs The list of STRs to be looked for in the profiles DNA sequences
      */
-    public void createProfileSTRs ( Profile profile, String[] allSTRs ) {    
-        
-        String sequence1 = profile.getSequence1();
+    public void createProfileSTRs ( Profile profile, String[] allSTRs ) {
+
+       
+        /* WRITE YOUR CODE HERE */
+        String sequence1 = profile.getSequence1(); //get DNA sequence from profile
         String sequence2 = profile.getSequence2();
-
-        int n = allSTRs.length;
         
-        STR[] S1_STRs = new STR[n];
-        STR[] S2_STRs = new STR[n];
-
-        for (int i = 0; i<n; i++){
-            S1_STRs[i] = findSTRInSequence(sequence1, allSTRs[i]);
-            S2_STRs[i] = findSTRInSequence(sequence2, allSTRs[i]);                        
+        STR[] S1_STRs = new STR[allSTRs.length]; //create two arrays to store STR objects 
+        STR[] S2_STRs = new STR[allSTRs.length];
+        //for loop to iterate each element in array
+        for(int i = 0; i < allSTRs.length; i++) {
+           String STR = allSTRs[i]; //gets i-th element from array and assigns it to STR
+           STR s1 = findSTRInSequence(sequence1, STR); //call findSTRinSequence to find lonest repeats in sequences
+           STR s2 = findSTRInSequence(sequence2, STR);
+           S1_STRs[i] = s1; //create STR objects and store them in arrays 
+           S2_STRs[i] = s2;
         }
-
-        profile.setS1_STRs(S1_STRs);
+        profile.setS1_STRs(S1_STRs); //set arrays in profile
         profile.setS2_STRs(S2_STRs);
 
-    }
+        
+
+            
+        }
+      
+
+    
 
     /**
      * Call createProfileSTRs() for each profile in the database.
      */
     public void createDatabaseSTRs() {
 
-        for (int i = 0; i<database.length; i++){
-            createProfileSTRs(database[i], STRsOfInterest);
-        }
-
         /* WRITE YOUR CODE HERE */
+        for(int i = 0; i < database.length; i++) {
+            createProfileSTRs(database[i], STRsOfInterest); //
+        }
 
     }
     
@@ -267,24 +260,18 @@ public class DNA {
      * @param s2 STR array from another profile.
      * @return Returns true if the objects in the arrays are a complete match, otherwise false
      */
-    public boolean identicalSTRs ( STR[] s1, STR[] s2 ) {
+   public boolean identicalSTRs ( STR[] s1, STR[] s2 ) {
 
-        if (s1.length!=s2.length || s1==null || s2==null){
+        /* WRITE YOUR CODE HERE */
+        if(s1 == null || s2 == null || s1.length != s2.length) {
             return false;
-        }
-
-        int n = s1.length;
-        for (int i = 0; i<n; i++){
-            String tempstr1 = s1[i].getSTR();
-            String tempstr2 = s2[i].getSTR();
-            int tempnor1 = s1[i].getRepeats();
-            int tempnor2 = s2[i].getRepeats();
-
-            if (!(tempnor1==tempnor2 && tempstr1.equals(tempstr2))){
-                return false;
-            }
-
-        }
+       }
+       for(int i = 0; i < s1.length; i++) {
+           if (s1[i].getRepeats() != s2[i].getRepeats()) {
+               return false;
+           }
+       }
+        
 
         return true; // update the return value
     }
@@ -301,18 +288,26 @@ public class DNA {
      */
     public ArrayList<Profile> findMatchingProfiles ( STR[] unknownProfileS1_STRs ) {
 
-        ArrayList<Profile> alp = new ArrayList<Profile>();
-
-        for (int i = 0; i<database.length; i++){
-            STR [] tempS1_STRs = database[i].getS1_STRs();
-            if (identicalSTRs(tempS1_STRs, unknownProfileS1_STRs)){
-                alp.add(database[i]);
+        /* WRITE YOUR CODE HERE */
+        ArrayList<Profile> matchingProfiles = new ArrayList<>(); //initializes ArrayList
+        for (Profile profile : database) {
+            STR[] profileS1_STRs = profile.getS1_STRs();
+            boolean matchFound = false;
+    
+            if (identicalSTRs(profileS1_STRs, unknownProfileS1_STRs)) {
+                matchFound = true;
             }
-            
+    
+            if (matchFound) {
+                matchingProfiles.add(profile);
+            }
         }
+    
+    return matchingProfiles; // update the return value
+}   
 
-	return alp; // update the return value
-    }
+	 
+    
 
     /**
      * 
@@ -358,159 +353,77 @@ public class DNA {
      *                 other parent passed down
      * @return Returns the array of profiles that are related
      */
-    public ArrayList<Profile> findPossibleParents ( STR[] S1_STRs, STR[] S2_STRs ) { // S1-STRs and S2_STRs are the inherited from first parent and inherited from second parent
+    public ArrayList<Profile> findPossibleParents ( STR[] S1_STRs, STR[] S2_STRs ) {
 
         /* FIX THIS METHOD */
-
-         ArrayList<Profile> possibleParent1 = new ArrayList<>();
+        ArrayList<Profile> possibleParent1 = new ArrayList<>();
          ArrayList<Profile> possibleParent2 = new ArrayList<>();
 
          for ( int i = 0; i < database.length; i++ ) {
+            //green checkmark is parent 2, yellow checkmark is parent 1
 
-            // S1 and S1 is parent 1?
             if (identicalSTRs(database[i].getS1_STRs(), S1_STRs)) {
                 possibleParent1.add(database[i]);
             }
-
-            // S1 and S2 (top right) is parent 2 [green checkmark] (25)
-            // if child
             if (identicalSTRs(database[i].getS1_STRs(), S2_STRs)) {
-                possibleParent2.add(database[i]);
+                 possibleParent2.add(database[i]);
              }
-
-             // S1 and S2 (bottom left) is parent 1 [yellow mark] (20)
              if (identicalSTRs(database[i].getS2_STRs(), S1_STRs)) {
-                possibleParent1.add(database[i]);
+                 possibleParent1.add(database[i]);
              }
-
-             // S2 and S2 is parent 2?
              if (identicalSTRs(database[i].getS2_STRs(), S2_STRs)) {
-                possibleParent2.add(database[i]);
+                 possibleParent2.add(database[i]);
              }
         }
 
         ArrayList<Profile> parentList = new ArrayList<>();
-
-         for ( int p1 = 0; p1 < possibleParent1.size(); p1++ ) {
+        for ( int p1 = 0; p1 < possibleParent1.size(); p1++ ) {
 
             for ( int p2 = 0; p2 < possibleParent2.size(); p2++ ) {
-
                 if ( !possibleParent1.get(p1).equals(possibleParent2.get(p2)) ) {
-
-                    // NAMAN WAY - 63/80
-                    /*
-                    if ( punnetSquare(
-                        possibleParent2.get(p2).getS2_STRs(), S1_STRs, 
+                    if (punnetSquare(
+                        possibleParent2.get(p2).getS1_STRs(), S1_STRs,
                         possibleParent1.get(p1).getS1_STRs(), S1_STRs)) 
                     {
                         parentList.add(possibleParent1.get(p1));
                         parentList.add(possibleParent2.get(p2));
                     } 
                     else if (punnetSquare(
-                            possibleParent2.get(p2).getS2_STRs(), S2_STRs, 
-                            possibleParent1.get(p1).getS1_STRs(), S2_STRs)) 
-                    {
-                        parentList.add(possibleParent1.get(p1));
-                        parentList.add(possibleParent2.get(p2));
-                    }
-                    else if (punnetSquare(
-                            possibleParent2.get(p2).getS2_STRs(), S2_STRs, 
-                            possibleParent1.get(p1).getS1_STRs(), S1_STRs)) 
+                        possibleParent2.get(p2).getS2_STRs(), S2_STRs, 
+                        possibleParent1.get(p1).getS2_STRs(), S2_STRs)) 
                     {
                         parentList.add(possibleParent1.get(p1));
                         parentList.add(possibleParent2.get(p2));
                     } 
                     else if (punnetSquare(
-                            possibleParent2.get(p2).getS2_STRs(), S1_STRs, 
-                            possibleParent1.get(p1).getS1_STRs(), S2_STRs))
-                    {
-                        parentList.add(possibleParent1.get(p1));
-                        parentList.add(possibleParent2.get(p2));
-                    }
-                    */
-                
-                    
-                    // HET WAY - 73/80
-
-                    /*
-                    
-                    if ( punnetSquare(
-                        possibleParent2.get(p2).getS1_STRs(), S1_STRs, 
+                        possibleParent2.get(p2).getS2_STRs(), S2_STRs, 
                         possibleParent1.get(p1).getS1_STRs(), S1_STRs)) 
                     {
                         parentList.add(possibleParent1.get(p1));
                         parentList.add(possibleParent2.get(p2));
                     } 
                     else if (punnetSquare(
-                            possibleParent2.get(p2).getS2_STRs(), S2_STRs, 
-                            possibleParent1.get(p1).getS2_STRs(), S2_STRs)) 
+                        possibleParent2.get(p2).getS1_STRs(), S1_STRs,
+                        possibleParent1.get(p1).getS2_STRs(), S2_STRs)) 
                     {
                         parentList.add(possibleParent1.get(p1));
                         parentList.add(possibleParent2.get(p2));
                     }
-                    else if (punnetSquare(
-                            possibleParent2.get(p2).getS2_STRs(), S2_STRs, 
-                            possibleParent1.get(p1).getS1_STRs(), S1_STRs)) 
-                    {
-                        parentList.add(possibleParent1.get(p1));
-                        parentList.add(possibleParent2.get(p2));
-                    } 
-                    else if (punnetSquare(
-                            possibleParent2.get(p2).getS1_STRs(), S1_STRs, 
-                            possibleParent1.get(p1).getS2_STRs(), S2_STRs))
-                    {
-                        parentList.add(possibleParent1.get(p1));
-                        parentList.add(possibleParent2.get(p2));
-                    }
-                    */
-
-                    if ( punnetSquare(
-                        possibleParent2.get(p2).getS2_STRs(), S1_STRs, 
-                        possibleParent1.get(p1).getS1_STRs(), S1_STRs)) 
-                    {
-                        parentList.add(possibleParent1.get(p1));
-                        parentList.add(possibleParent2.get(p2));
-                    } 
-                    else if (punnetSquare(
-                            possibleParent2.get(p2).getS2_STRs(), S2_STRs, 
-                            possibleParent1.get(p1).getS1_STRs(), S2_STRs)) 
-                    {
-                        parentList.add(possibleParent1.get(p1));
-                        parentList.add(possibleParent2.get(p2));
-                    }
-                    else if (punnetSquare(
-                            possibleParent2.get(p2).getS2_STRs(), S2_STRs, 
-                            possibleParent1.get(p1).getS1_STRs(), S1_STRs)) 
-                    {
-                        parentList.add(possibleParent1.get(p1));
-                        parentList.add(possibleParent2.get(p2));
-                    } 
-                    else if (punnetSquare(
-                            possibleParent2.get(p2).getS2_STRs(), S1_STRs, 
-                            possibleParent1.get(p1).getS1_STRs(), S2_STRs))
-                    {
-                        parentList.add(possibleParent1.get(p1));
-                        parentList.add(possibleParent2.get(p2));
-                    }
-
                     
-                }
             }
         }
+    }
+
+         
         return parentList;
-    }
+         
+    }        
+    
+          
+       
+                
+         
 
-    /**
-     * Getter for the database/profiles instance variable
-     * 
-     * @return The database instance variable
-     */
-    public Profile[] getDatabase() {
-
-        /* DO NOT EDIT */
-
-        return database;
-    }
 
     /**
      * Getter for allSTRs instance variable
@@ -522,5 +435,10 @@ public class DNA {
         /* DO NOT EDIT */
 
         return STRsOfInterest;
+    }
+
+    public Profile[] getDatabase() {
+        /* DO NOT EDIT */
+        return database;
     }
 }
